603aa091c9606ba394c9cbfe19a6a3033aa89dc4
ludvan <ludvan@student.chalmers.se>
2022-09-14 15:04:11 +0200

    Notifications on notification branch

--- app/src/main/java/com/firstapp/jakku/MainActivity.java
+++ app/src/main/java/com/firstapp/jakku/MainActivity.java
@@ -1,73 +1,94 @@
 package com.firstapp.jakku;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
-import android.content.DialogInterface;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Button;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView info;
-    Button settings;
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        info=(TextView) findViewById(R.id.tv_info);
-        settings=(Button) findViewById(R.id.b_settings);
+        button = findViewById(R.id.button);
 
-        //code for active button
- /*     settings.setOnClickListener(new View.OnClickListener() {
+
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                System.out.println("Button pressed");
-       //         Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-
-                if (info.getVisibility()==View.VISIBLE) {
-                    info.setVisibility(View.INVISIBLE);
-                }else {
-                    info.setVisibility(View.VISIBLE);
-                }
-
+            public void onClick(View v){
+                notificationSetup();
             }
-        }
-
-        ) */
-        ;
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.settingsmenu,menu);
-        return true;
+    private void notificationSetup(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){ //if Android 8 or newer
+            NotificationChannel channel = new NotificationChannel("Test", "Testing", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"Test");
+        Date currentTime = Calendar.getInstance().getTime();
+        builder.setContentTitle("Test_title");
+        builder.setContentText(currentTime.toString());
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+        managerCompat.notify(1,builder.build());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    private void setAlarm(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.MINUTE, 29);
+        calendar.set(Calendar.SECOND, 0);
 
-        int id= item.getItemId();
+        if (calendar.getTime().compareTo(new Date()) < 0)
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
 
-        //this handles tab on activity, duplicate for every new item in the menu
-        if(id == R.id.sportspref){
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
+        Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        if (alarmManager != null) {
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
         }
 
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    public class NotificationReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            /*
+            NotificationHelper notificationHelper = new NotificationHelper(context);
+            notificationHelper.createNotification();
+
+             */
+
+            notificationSetup();
+        }
     }
 }
 
