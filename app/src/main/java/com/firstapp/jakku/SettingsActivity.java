cb1d66906f8e7afb403cb2071bb8b493c334937a
chrmeis <chrmeis@student.chalmers.se>
2022-09-14 15:10:52 +0200

    so many changes...

--- app/src/main/java/com/firstapp/jakku/SettingsActivity.java
+++ app/src/main/java/com/firstapp/jakku/SettingsActivity.java
@@ -2,19 +2,21 @@ package com.firstapp.jakku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
     Button back;
-
+    Switch sportssettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+/* //manuel "back" button
         back=(Button) findViewById(R.id.b_backFromSettings);
 
         back.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +26,21 @@ public class SettingsActivity extends AppCompatActivity {
                finish();
             }
         });
+ */
+        sportssettings = findViewById(R.id.sw_sports);
+        sportssettings.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b) {
+                    // The toggle is enabled
+                    System.out.println("Toggle enabled");
+                } else {
+                    // The toggle is disabled
+                    System.out.println("Toggle disabled");
+                }
+            }
+        });
+
     }
 }
