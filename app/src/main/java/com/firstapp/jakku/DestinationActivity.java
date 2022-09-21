708e177cbec2c61fe04e4c24aadc456fc13d4929
ludvan <ludvan@student.chalmers.se>
2022-09-21 11:10:10 +0200

    Main only shows button that starts the notification function.

--- app/src/main/java/com/firstapp/jakku/DestinationActivity.java
+++ app/src/main/java/com/firstapp/jakku/DestinationActivity.java
@@ -1,6 +1,8 @@
 package com.firstapp.jakku;
 
 
+import androidx.appcompat.app.AppCompatActivity;
+
 //this should be changed to the target of the notification, not yet implemented
-public class DestinationActivity {
+public class DestinationActivity extends AppCompatActivity {
 }
