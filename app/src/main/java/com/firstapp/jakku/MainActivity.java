d8868ab7cd0efac7350ebd2e1707b980d1d1d64b
ludvan <ludvan@student.chalmers.se>
2022-09-28 13:24:14 +0200

    Revert "Merge branch 'main' into Notification"
    This reverts commit 3d0a2cf0357f02f978baa31cce4d427f26425c6c, reversing
    changes made to 0638673cd8c042d4b01a28f84dc5bf9769d0b668.

--- app/src/main/java/com/firstapp/jakku/MainActivity.java
+++ app/src/main/java/com/firstapp/jakku/MainActivity.java
@@ -26,20 +26,9 @@ import com.google.android.material.timepicker.TimeFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
 
-    static TextView info;
-//    Button settings;
-    //Weather weather;
-
-    public static void updateInfo(String string){
-        info.setText(string);
-    }
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,60 +55,6 @@ public class MainActivity extends AppCompatActivity {
             NotificationChannel channel = new NotificationChannel("Test", "Testing", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
-        info=(TextView) findViewById(R.id.textView2);
-//        settings=(Button) findViewById(R.id.b_settings);
-        //JSONArray jsonArray = new JSONArray();
-        //final String[] string = {"fail"};
-        //final String[] extra = {"Slow"};
-        TalkToServer varName = new TalkToServer(); //pass parameters if you need to the constructor
-        varName.execute();
-        /*new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Test1");
-                info.setText("fail");
-                //weather = new Weather();
-                //JSONArray jsonArray  = weather.getJsonArray();
-                //String  string = weather.temperature();
-                // After getting the result
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Post the result to the main thread
-                        System.out.println("Test2");
-                       // info.setText(string);
-                        //extra[0] = string;
-                    }
-                });
-            }
-        }).start();
-        */
-        /*try {
-            //String string = ((JSONArray) ((JSONObject) jsonArray.get(0)).get("parameters"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }*/
-        //info.setText(extra[0]);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.settingsmenu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        int id= item.getItemId();
-
-        //this handles tab on activity, duplicate for every new item in the menu
-        if(id == R.id.trainingspref){
-            Intent intent = new Intent(MainActivity.this, TrainingActivity.class);
-            startActivity(intent);
-            finish();
-            return true;
         }
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"Test");
