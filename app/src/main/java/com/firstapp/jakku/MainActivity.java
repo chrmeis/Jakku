3c9b923388fdc254ed43a21ae4a7fc114f63552e
chrmeis <chrmeis@student.chalmers.se>
2022-10-17 14:40:07 +0200

    little more readability

--- app/src/main/java/com/firstapp/jakku/MainActivity.java
+++ app/src/main/java/com/firstapp/jakku/MainActivity.java
@@ -2,43 +2,19 @@ package com.firstapp.jakku;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.app.AlarmManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.firstapp.jakku.databinding.ActivityMainBinding;
-import com.google.android.material.timepicker.MaterialTimePicker;
-import com.google.android.material.timepicker.TimeFormat;
-
-import java.util.Calendar;
-import java.util.Date;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -71,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String T_FREQUENCY = "t_how_often";
     private static final String T_DURATION = "t_how_long";
 
-    TextView info;
+    TextView weatherText;
     ImageView weatherSymbol;
 //    Button settings;
     //Weather weather;
@@ -79,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public static void updateInfo(String string){
-        info.setText(string);
+    public void updateInfo(String string){
+        weatherText.setText(string);
     }
 
     public static void updateShedule(){
@@ -113,17 +89,18 @@ public class MainActivity extends AppCompatActivity {
         sat = findViewById(R.id.b_saturday);
         sun = findViewById(R.id.b_sunday);
 
-        info=(TextView) findViewById(R.id.textView2);
-//        settings=(Button) findViewById(R.id.b_settings);
+        weatherText = findViewById(R.id.textView2);
 
         SharedPreferences SP_Study = getSharedPreferences(SHARED_SPREFS, MODE_PRIVATE);
         int s_frequency = SP_Study.getInt(STUDY_FREQUENCY, 4);
         int s_duration = SP_Study.getInt(STUDY_DURATION, 1);
         Schema.set_studypref(s_frequency,s_duration);
 
-        System.out.println("\n\nshared study in main:");
+/*        System.out.println("\n\nshared study in main:");
         System.out.println("s_frequency: "+s_frequency);
         System.out.println("s_duration: "+s_duration+"\n\n");
+
+ */
 
 
         SharedPreferences SP_train = getSharedPreferences(SHARED_TPREFS, MODE_PRIVATE);
@@ -133,9 +110,11 @@ public class MainActivity extends AppCompatActivity {
 
         Schema.set_trainingpref(t_frequency, t_duration);
 
-        System.out.println("\n\nshared training in main:");
+/*        System.out.println("\n\nshared training in main:");
         System.out.println("t_frequency: "+t_frequency);
         System.out.println("t_duration: "+t_duration+"\n\n");
+
+ */
 
         weatherSymbol.setImageResource(R.drawable.rain);
         ExecutorService executor = Executors.newSingleThreadExecutor();
@@ -234,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //putting schema into homescreen
+        //setting variables for schema into homescreen
         Schema.set_weekday();
         Schema.set_studypref(s_frequency,s_duration);
         Schema.set_trainingpref(t_frequency,t_duration);
